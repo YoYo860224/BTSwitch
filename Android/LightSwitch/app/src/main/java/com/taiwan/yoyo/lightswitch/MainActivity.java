bd83227d8ce373ca129b9ad81f4a00ab076dd868
YoCheng <xyox860224@hotmail.com>
2018-08-25 02:07:16 +0800

    Localize, drak theme, auto connect.

--- Android/LightSwitch/app/src/main/java/com/taiwan/yoyo/lightswitch/MainActivity.java
+++ Android/LightSwitch/app/src/main/java/com/taiwan/yoyo/lightswitch/MainActivity.java
@@ -1,8 +1,16 @@
 package com.taiwan.yoyo.lightswitch;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -48,13 +56,36 @@ public class MainActivity extends Activity {
             Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnBTon, 1);
         }
+        else {
+            pairedDevicesList();
 
+            SharedPreferences preferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+            if (preferences.getString("blueT", "") != "") {
+                String address = preferences.getString("blueT", "");
+                goToControl(address);
+            }
+        }
         btnPaired.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 pairedDevicesList();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                SharedPreferences preferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+                if (preferences.getString("blueT", "") != "") {
+                    String address = preferences.getString("blueT", "");
+                    goToControl(address);
+                }
+            }
+        }
     }
 
     private void pairedDevicesList() {
@@ -69,10 +100,32 @@ public class MainActivity extends Activity {
             Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
         }
 
-        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-        devicelist.setAdapter(adapter);
-        devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, list) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the Item from ListView
+                View view = super.getView(position, convertView, parent);
 
+                // Initialize a TextView for ListView each Item
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+
+                // Set the text color of TextView (ListView Item)
+                tv.setTextColor(Color.WHITE);
+
+                // Generate ListView Item using TextView
+                return view;
+            }
+        };
+
+        devicelist.setAdapter(arrayAdapter);
+        devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+    }
+
+    private void goToControl(String bluetoothAdress) {
+        Intent i = new Intent(MainActivity.this, ControlActivity.class);
+        i.putExtra(EXTRA_ADDRESS, bluetoothAdress);
+        startActivity(i);
     }
 
     private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
@@ -82,12 +135,13 @@ public class MainActivity extends Activity {
             // Get the device MAC address, the last 17 chars in the View
             String info = ((TextView) v).getText().toString();
             String address = info.substring(info.length() - 17);
-            // Make an intent to start next activity.
-            // Intent i = new Intent(DeviceList.this, ledControl.class);
-            Intent i = new Intent(MainActivity.this, ControlActivity.class);
-            //Change the activity.
-            i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
-            startActivity(i);
+
+            SharedPreferences preferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("blueT", address);
+            editor.commit();
+
+            goToControl(address);
         }
     };
 }
